6b2674eb9d956cf04aa1b689626ed382dd2e775a
Andres <andresngomez@live.com>
2025-11-05 18:11:15 -0600

    cors fix?

--- src/functions/corsResponse.js
+++ src/functions/corsResponse.js
@@ -1,5 +1,10 @@
 function corsResponse(response) {
     response.headers = response.headers || {};
-    response.headers['Access-Control-Allow-Origin'] = '*';
+    response.headers['Access-Control-Allow-Origin'] = 'https://witty-beach-062ca4b10.3.azurestaticapps.net';
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS';
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type';
+    response.headers['Access-Control-Allow-Credentials'] = 'true';
     return response;
 }
+
+module.exports = { corsResponse };
